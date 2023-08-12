e96be37a4810518df23596537aee803a03b83b86
kenya194 <ernestbv553@gmail.com>
2023-08-12 15:55:21 +0000

    init

---
+++ react/expenditure_tracker/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Header = () => {
+    return ( 
+        <div>
+            <h2>EXPENDITURE TRACKER</h2>
+        </div>
+     );
+}
+ 
+export default Header;
