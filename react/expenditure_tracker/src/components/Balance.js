f9ec0694a58e0e0d08218a2afad98877ee9cc6a1
kenya194 <ernestbv553@gmail.com>
2023-08-12 20:32:14 +0000

    building components

--- react/expenditure_tracker/src/components/Balance.js
+++ react/expenditure_tracker/src/components/Balance.js
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Balance() {
   return (
-    <div>Balance</div>
+    <div>
+    <h3>Your Balance</h3>
+    
+    <h1 id='balance'>$0.00</h1>
+    </div>
   )
 }
---
+++ react/expenditure_tracker/src/components/incomeExpense.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+
+const incomeExpense = () => {
+    return (
+        <div>
+            <h4>Income</h4>
+            <h3>$0.00</h3>
+
+            <h4>Expense</h4>
+            <h3>$0.00</h3>
+        </div>
+      );
+}
+ 
+export default incomeExpense ;
