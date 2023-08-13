09423daf9a9fc9ee17919e94c612b35520e7e7ed
kenya194 <ernestbv553@gmail.com>
2023-08-13 12:35:29 +0000

    Built all components

---
+++ react/expenditure_tracker/src/components/AddTransaction.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddTransaction = () => {
+    return ( 
+        <div>
+            <button type="button">Add Transaction</button>
+        </div>
+     );
+}
+ 
+export default AddTransaction;
