96f452e1ac35a14123a7bb8f7af2323088c15150
kenya194 <ernestbv553@gmail.com>
2023-08-14 08:31:57 +0000

    styling

--- react/expenditure_tracker/src/components/History.js
+++ react/expenditure_tracker/src/components/History.js
@@ -6,8 +6,11 @@ const History = () => {
         <div>
             <h1>History</h1>
 
+            <div className="history_output">
             <h3>Cash</h3>
             <h4>+ $500</h4>
+            </div>
+
             <h3>Book</h3>
             <h4>- $200</h4>
         
--- react/expenditure_tracker/src/components/incomeExpense.js
+++ react/expenditure_tracker/src/components/incomeExpense.js
@@ -3,11 +3,16 @@ import React from "react";
 const IncomeExpense = () => {
     return ( 
         <div className="income_Expense">
+
+            <div>
             <h2>Income</h2>
             <p className="Inc_ome">$0.00</p>
+            </div>
 
+            <div>
             <h2>Expenditure</h2>
             <p className="ex_pense">$0.00</p>
+            </div>
         </div>
      );
 }
