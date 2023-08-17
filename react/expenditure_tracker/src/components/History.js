e64eb672956045fb45a094fc6dcfd87dab1798e8
kenya194 <ernestbv553@gmail.com>
2023-08-17 01:53:00 +0000

    API

--- react/expenditure_tracker/src/components/History.js
+++ react/expenditure_tracker/src/components/History.js
@@ -6,10 +6,16 @@ const History = () => {
         <div>
             <h1>History</h1>
 
-            <div className="history_output">
+            <ul id="list">
+                <li className="plus">Cash <span>+ $500</span>
+                <button className="delete_btn">X</button>
+                </li>
+            </ul>
+
+            {/* <div className="history_output">
             <h3>Cash</h3>
             <h4>+ $500</h4>
-            </div>
+            </div> */}
         
         </div>
      );
