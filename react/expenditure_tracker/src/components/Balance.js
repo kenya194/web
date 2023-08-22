06ece5b38fadff40c407674a887a83bf19551cb2
kenya194 <ernestbv553@gmail.com>
2023-08-22 20:40:54 +0000

    commit

--- react/expenditure_tracker/src/components/Balance.js
+++ react/expenditure_tracker/src/components/Balance.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from "../context/GlobalState";
 
 export default function Balance() {
+   const {transactions} = useContext(GlobalContext);
+
+   const amounts = transactions.map(transaction => transaction.amount);
+   const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+   // looping through the amount array and summing up the total transaction to initial the total balance.
+
   return (
     <div className='balance'>
     <h2>Your Balance</h2>
     
-    <h2 id='balance'>$0.00</h2>
+    <h2> ${total}</h2>
     </div>
   )
 }
